06a30aa2712d6533e28902fe2fa473d10f994590
eshrak16 <eshraksolkar16@gmail.com>
2026-09-17 21:11:00 +0530

    Add UI design components

--- mini-projects/02-ui-design/src/components/section1/navbar.jsx
+++ mini-projects/02-ui-design/src/components/section1/navbar.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between px-16 py-6'>
-        <h4 className='bg-black rounded-full px-6 py-2 text-white uppercase py-6 px-16'>target audience</h4>
-        <button className='bg-gray-200 '>digital banking platform</button>
-
+    <div className='flex items-center justify-between py-8 px-18'>
+        <h4 className='bg-black text-white uppercase px-6 py-2 rounded-full'>Target Audience</h4>
+        <button className='bg-gray-200 px-6 py-2 uppercase rounded-full tracking-widest text-sm'>digital Banking Platform</button>
     </div>
   )
 }
--- mini-projects/02-ui-design/src/components/section1/section1.jsx
+++ mini-projects/02-ui-design/src/components/section1/section1.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import Navbar from '../section1/navbar'
-const section1 = () => {
+import Navbar from './Navbar'
+import Page1Content from './Page1Content'
+
+const Section1 = (props) => {
   return (
-    <div className='h-screen w-full'>
-      <Navbar />
+    <div className='h-screen w-full '>
+        <Navbar />
+        <Page1Content users={props.users} />
     </div>
   )
 }
 
-export default section1
+export default Section1
